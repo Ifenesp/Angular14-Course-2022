9baf4a0fb320245694b7281e5dfac830a7403a6a
Ifenesp <nefi_jari@hotmail.com>
2022-08-13 17:14:10 -0400

    [12-08-2022 - Task 1] Arrow example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test-Angular';
-}
+
+  constructor()
+  {
+    console.log('wena perro');
+  }
+
+  public getName(name: string): string
+  {
+    return "hi" + name;
+  }
+
+  //ejemplos funcion flecha
+  //const getName = (name: string) => {return "hi" + name}
+  //const getName = (name: string) => "hi" + name
+  //const getName = () => "hi"
+  //const getNam1 = (name: string, lastName: string) => lastname
+ }
