b1c78c487fe56ba5d028acb6a269a84155fd1534
Ifenesp <nefi_jari@hotmail.com>
2022-08-19 17:56:11 -0400

    [19-08-2022 Task 6] Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IPerson } from './person/person.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test-Angular';
+  
+  //en este caso el ! sirve para inicializar la variable person sin datos declarados
+  public person!: IPerson;
+
+  public student: string = 'example Nefi';
+
+  public a: number= 0;
+  public b: number= 0;
+
+  getPrint(event:IPerson)
+  {
+    console.log('parent component effect:', event)
+    this.person = event;
+  }
 }
---
+++ src/app/person/person.model.ts
@@ -0,0 +1,6 @@
+export interface IPerson 
+{
+  name:string;
+  lastName:string;
+  age:number;
+}
